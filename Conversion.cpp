093f3ac8a08f242a029e88e4569b3d5fe3932e26
Abdul Rafay <malik.rafay453@gmail.com>
2021-01-31 12:49:30 +0500

    Update Conversion.cpp

--- Conversion.cpp
+++ Conversion.cpp
@@ -1,14 +1,57 @@
-#include<stdio.h>
-#include<conio.h>
-
+#include <stdio.h>
 int main()
 {
-	char c;
-	printf("Enter a character:");
-	scanf("%c",&c);
-	
-	// %d dispays the integer value of a character
-	// %c displays the actual character
-	printf("ASCII value of %c=%d",c,c);
+	float fh,cl,K;
+	int ch;
+	printf("\n 1. Convert temp from Fahrenheit to Celsius.");
+	printf("\n 2. Convert temp from Celcius to Fahrenheit.");
+	printf("\n 3. Convert temp from Celcius to Kelvin.");
+	printf("\n 4. Convert temp from Kelvin to Celcius.");
+	printf("\n 5. Convert temp from Fahrenheit to Kelvin.");
+	printf("\n 6. Convert temp from Kelvin to Fahrenheit.");
+	printf("\n Enter Your Choice: ");
+	scanf("%d",&ch);
+	if(ch==1)
+	{
+		printf("\n Enter Temp in Fahrenheit:  ");
+		scanf("%f",&fh);
+		cl=(fh-32/1.8);
+		printf("Temp in Celcius: %f: ",cl);
+	}
+	else if(ch==2)
+	{
+		printf("\n Enter Temp in Celcius:  ");
+		scanf("%f",&cl);
+		fh=(cl*1.8+32);
+		printf("Temp in Fahrenheit: %f: ",fh);
+	}
+	if(ch==3)
+	{
+		printf("\n Enter Temp in Celsius:  ");
+		scanf("%f",&cl);
+		K=(cl+273);
+		printf("Temp in Kelvin: %f: ",K);
+	}
+	else if(ch==4)
+	{
+		printf("\n Enter Temp in Kelvin:  ");
+		scanf("%f",&K);
+		cl=(K-273);
+		printf("Temp in Celcius: %f: ",cl);
+	}
+	if(ch==5)
+	{
+		printf("\n Enter Temp in Fahrenheit:  ");
+		scanf("%f",&fh);
+		K=0.555*(fh-32)+273;
+		printf("Temp in Kelvin: %f: ",K);
+	}
+	else if(ch==6)
+	{
+		printf("\n Enter Temp in Kelvin:  ");
+		scanf("%f",&K);
+		fh= 1.8*(K-273)+32;
+		printf("Temp in Fahrenheit: %f: ",fh);
+	}
 	return 0;
 }
